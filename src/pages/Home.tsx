43f6e4cafda737de7cc74d40f772995d30a66ce4
MarkinAO <habar77@yandex.ru>
2024-12-01 19:58:34 +0700

    [~] Актуализация текстов

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -58,7 +58,7 @@ export default function Home() {
             </div>            
             <Portfolio />
             <Skills />
-            {/* <SoftSkills /> */}
+            <SoftSkills />
           </div>
         </div>
       </main>
